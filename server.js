4da2fa08224d3bda2254284ca68a50374fe7a592
iresh96 <ireshmadushanka03@gmail.com>
2024-11-02 16:14:50 +0100

    updates

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv");
-const colors = require("colors");
-const morgan = require("morgan");
-const connectDB = require("./config/db");
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config();
 connectDB();
 
-const transactions = require("./routes/transactions");
+const transactions = require('./routes/transactions');
 const app = express();
 app.use(express.json());
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-app.use("/api/v1/transactions", transactions);
+app.use('/api/v1/transactions', transactions);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
 
